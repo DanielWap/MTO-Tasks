01b5c7807aec5572d4366fbb8c7da7706b6892fc
DanielWap <daniel.waprzko@outlook.com>
2022-11-20 11:34:24 +0100

    IMproved script and input file

--- lab5/main.c
+++ lab5/main.c
@@ -4,7 +4,8 @@
 
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] == 'X' && format_string[i+2] == 'g')){
+		if((format_string[i] == '#') && (format_string[i+1] > '0' && format_string[1] <= '9' ) && (format_string[i+2] == 'g')){
+			int X = (char) format_string[i+1];
 			i++;
 			printf("%Xd",param);
 	}
