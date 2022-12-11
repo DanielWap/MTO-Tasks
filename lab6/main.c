32c516575e95bbe41a918278e1910f9d471aa518
DanielWap <daniel.waprzko@outlook.com>
2022-12-11 11:36:40 +0100

    LAB 6 format changes
    LAB 6 format changes

--- lab6/main.c
+++ lab6/main.c
@@ -4,7 +4,7 @@
 
 int my_printf(char *format_string, char *param) {
 	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#') && (format_string[i+1] > '0' && format_string[1] <= '9' ) && (format_string[i+2] == 'g')){
+		if((format_string[i] == '#')&&(format_string[i+1] > '0'&&format_string[1] <= '9' )&&(format_string[i+2] == 'g')) {
 			int X = (char) format_string[i+1];
 			i++;
 			printf("%Xd",param);
@@ -14,7 +14,7 @@ int my_printf(char *format_string, char *param) {
 				int x = format_string[i] - 1;
 				putchar((char)(x));
 			}
-			else if(format_string[i] == '0'){
+			else if(format_string[i] == '0') {
 				int x = 9;
 				putchar((char)(x));
 			}
