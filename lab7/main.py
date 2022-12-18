a4f0b33359c6fa3a4cc4c139a16371bcc20c1d7a
DanielWap <daniel.waprzko@outlook.com>
2022-12-18 13:36:19 +0100

    Prepared Python Script

--- lab7/main.py
+++ lab7/main.py
@@ -1,35 +1,34 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+#!/usr/bin/env python3
 
-int my_printf(char *format_string, char *param){
-	for(int i=0;i<strlen(format_string);i++){
-		if((format_string[i] == '#')&&(format_string[i+1] > '0'&&format_string[1] <= '9' )&&(format_string[i+2] == 'g')){
-			int X = (char) format_string[i+1];
-			i++;
-			printf("%Xd",param);
-		}
-		else{
-			if(format_string[i] > '0' && format_string[i] <= '9' ){
-				int x = format_string[i] - 1;
-				putchar((char)(x));
-			}
-			else if(format_string[i] == '0'){
-				int x = 9;
-				putchar((char)(x));
-			}
-			else
-				putchar((format_string[i]));
-		}	
-	}
-		puts("");
-		return 0;
-}
-int main(int argc, char *argv[]){
-	char buf[1024],buf2[1024];
-	while(gets(buf)){
-		gets(buf2);
-		my_printf(buf,buf2);
-	}
-	return 0;
-}
+import sys
+
+def convertHex(string):
+    converted = ''
+    for char in string:
+        if 'a' <= char <= 'f':
+            char = chr(ord(char)+6)
+        converted += char
+
+    return converted
+
+
+def my_printf(format_string, param):
+    shouldDo = True
+    for idx in range(0, len(format_string)):
+        if shouldDo:
+            if format_string[idx] == '#' and format_string[idx+1] == 'j' and param.isnumeric():
+                hexValue = f"{int(param):x}"
+                print(convertHex(hexValue), end="")
+
+                shouldDo = False
+            else:
+                print(format_string[idx], end="")
+        else:
+            shouldDo = True
+    print("")
+
+
+data = sys.stdin.readlines()
+
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i+1].rstrip())
